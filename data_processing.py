fd3e67eaf681a027d0706f5f76dbd696426804c6
carryforong <40648465+carryforong@users.noreply.github.com>
2019-11-15 22:31:40 +0800

    .

--- data_processing.py
+++ data_processing.py
@@ -1,6 +1,8 @@
 import torchvision
 import torch
-
+import cv2
+import PIL
+import numpy as np
 #Compose 这个类是用来管理各个transform的
 #ToTensor  类是实现：Convert a PIL Image or numpy.ndarray to tensor
 #Normalize  类是做数据归一化的
@@ -10,7 +12,7 @@ import torch
 #RandomHorizontalFlip  类也是比较常用的，是随机的图像水平(Horizonta)Horizonta翻转，通俗讲就是图像的左右对调
 #RandomVerticalFlip  类是随机的图像竖直(Vertical）翻转，通俗讲就是图像的上下对调
 
-#RandomResizedCrop  类主要用到3个参数：size、scale和ratio，总的来讲就是先做crop（用到scale和ratio），再resize到指定尺寸（用到size）。
+#RandomResizedCrop  类主要用到3个参数：size、scale和ratio，总的来讲就是先做crop（用到scale和ratio），再resize到指定尺寸（用到size）
 # 做crop的时候，其中心点坐标和长宽是由get_params方法得到的，在get_params方法中主要用到两个参数：scale和ratio，首先在scale限定的数值范
 # 围内随机生成一个数，用这个数乘以输入图像的面积作为crop后图像的面积；然后在ratio限定的数值范围内随机生成一个数，表示长宽的比值，根据这两个值就可以得到crop图像的长宽了
 
@@ -19,13 +21,39 @@ import torch
 #RandomRotation 类是随机旋转输入图像
 #Grayscale 类是用来将输入图像转成灰度图的
 
-train_augmentation = torchvision.transforms.Compose([torchvision.transforms.Resize(256),
-                                                    torchvision.transforms.RandomCrop(224),                                                                            
-                                                    torchvision.transofrms.RandomHorizontalFlip(),
+
+path="data\\class\\lable\\coal\\0.JPG"
+batch_size=32
+workers=3
+
+train_augmentation = torchvision.transforms.Compose([
+                                                    torchvision.transforms.Resize(256),
+                                                    torchvision.transforms.RandomCrop(224),
                                                     torchvision.transforms.ToTensor(),
-                                                    torchvision.Normalize([0.485, 0.456, -.406],[0.229, 0.224, 0.225])
                                                     ])
-train_loader = torch.utils.data.DataLoader(
-    custom_dataread(transform=train_augmentation),
+
+
+
+#train_augmentationj=接受到的数据为 PIL.Image.Image，一般为RGB；输出为归一化的数组，opencv读入图像为BGR图像（img_cv = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)）
+# img_PIL = PIL.Image.open(path).convert('RGB')
+# img1 = train_augmentation(img_PIL)
+# img_1 = img1.numpy()*255
+# img_1 = img_1.astype('uint8')
+# img_1 = np.transpose(img_1, (1,2,0))      
+# cv2.imshow('img_1', img_1)
+# cv2.waitKey()
+
+trainset = torchvision.datasets.ImageFolder(path,train_augmentation,)
+
+
+# # print(trainset.classes) #根据分的文件夹的名字来确定的类别
+# # print(trainset.class_to_idx) #按顺序为这些类别定义索引为0,1...
+# # print(trainset.imgs) #返回从所有文件夹中得到的图片的路径以及其类别
+
+
+# #shuffle是否打乱数据
+# #num_workers数据分为几个线程处理，默认为0
+
+train_loader = torch.utils.data.DataLoader(trainset,
     batch_size = batch_size, shuffle = True,
     num_workers = workers, pin_memory = True)
